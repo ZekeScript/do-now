cdc4f789682510dc1beecca175d9c78a74dd6d1a
estga <estiga27@gmail.com>
2022-05-20 19:40:55 -0300

    adding some coments, change some functions to arrow function

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,14 @@
 var rate, amount, mainCurrency, secondaryCurrency;
 const currencys = [
-	//this array contains all currency objects
-	{ name: 'dollar', peso: 117.43, dollar: 1, euro: 0.96, real: 5.06 }, //currency object
+	// This array contains all currency objects
+	{ name: 'dollar', peso: 117.43, dollar: 1, euro: 0.96, real: 5.06 }, // currency object
 	{ name: 'euro', peso: 122.26, dollar: 1.04, euro: 1, real: 5.27 },
 	{ name: 'real', peso: 23.2, dollar: 0.2, euro: 0.19, real: 1 },
 	{ name: 'peso', peso: 1, dollar: 0.0085, euro: 0.0082, real: 0.043 },
 ];
 
-//rotate animation
+// Rotate animation
 function refreshBtn() {
-	var currencyLeft = document.getElementById('currencyLeft');
-	var currencyRight = document.getElementById('currencyRight');
-
-	var newRightCurrencyValue = currencyLeft.value;
-	var newLeftCurrencyValue = currencyRight.value;
-	currencyLeft.value = newLeftCurrencyValue;
-	currencyRight.value = newRightCurrencyValue;
-	convertionFromLeft();
-
 	let rotacion = (360 / 100) * 2.5; // rotation time
 	setTimeout(() => {
 		document.getElementById('btnRefresh').classList.add('rotate');
@@ -28,82 +19,89 @@ function refreshBtn() {
 	}, rotacion);
 }
 
-function clean() {
-	floatingInputLeft = document.getElementById('floatingInputLeft');
-	floatingInputLeft.value = 0;
-	floatingInputRight = document.getElementById('floatingInputRight');
-	floatingInputRight.value = 0;
-}
-
-function changeLabelLeft() {
-	var labelLeft = document.getElementById('labelLeft');
-	if (currencyLeft.value != '') {
-		labelLeft.innerText = `Amount of ${currencyLeft.value}s`;
-		convertionFromLeft();
-	} else {
-		labelLeft.innerHTML = 'Amount';
-		InputLeft = document.getElementById('floatingInputLeft');
-		InputLeft.setAttribute('disabled', '');
-	}
-}
-function changeLabelRight() {
-	var labelRight = document.getElementById('labelRight');
-	if (currencyRight.value != '') {
-		labelRight.innerText = `Amount of ${currencyRight.value}s`;
-		convertionFromRight();
-	} else {
-		labelRight.innerHTML = 'Amount';
-		floatingInputRight = document.getElementById('floatingInputRight');
-		floatingInputRight.setAttribute('disabled', '');
-	}
-}
-function convertionFromLeft() {
-	amount = floatingInputLeft.value;
-	secondaryCurrency = currencyRight.value;
-	mainCurrency = currencys.find((currency) => currency.name === currencyLeft.value);
-    rate = mainCurrency[secondaryCurrency];
+/**
+ * Take the values of both currencies and the convertion amoun
+ * and save the values in the localStorage
+ */
+function saveValues() {
     localStorage.setItem('mainCurrency', JSON.stringify(mainCurrency));
-	localStorage.setItem('secondaryCurrency', secondaryCurrency);
-	localStorage.setItem('amount', amount);
-	floatingInputRight = document.getElementById('floatingInputRight');
-	floatingInputRight.value = (amount * rate).toFixed(2);
+    localStorage.setItem('secondaryCurrency', secondaryCurrency);
+    localStorage.setItem('amount', amount);    
 }
-function convertionFromRight() {
-	amount = floatingInputRight.value;
-	secondaryCurrency = currencyLeft.value;
-	mainCurrency = currencys.find((currency) => currency.name === currencyRight.value);
+
+/**
+ * Both functions (fromLef and fromRight) do the calculations for the conversion
+ * first takes the values ​​in both select
+ * then, look up the conversion rate using both currencies names
+ * then, writes the values ​​to localStorage
+ * and finally uses the attached rate and amount in the input to do the calculations for the conversion
+ */
+function fromLeft() {
+	amount = leftInput.value;
+	secondaryCurrency = rightCurrency.value;
+	mainCurrency = currencys.find((currency) => currency.name === leftCurrency.value);
     rate = mainCurrency[secondaryCurrency];
-    localStorage.setItem('mainCurrency', JSON.stringify(mainCurrency));
-	localStorage.setItem('secondaryCurrency', secondaryCurrency);
-	localStorage.setItem('amount', amount);
-	floatingInputLeft = document.getElementById('floatingInputLeft');
-	floatingInputLeft.value = (amount * rate).toFixed(2);
+    saveValues();
+	rightInput = document.getElementById('rightInput');
+	rightInput.value = (amount * rate).toFixed(2);
+}
+function fromRight() {
+	amount = rightInput.value;
+	secondaryCurrency = leftCurrency.value;
+	mainCurrency = currencys.find((currency) => currency.name === rightCurrency.value);
+    rate = mainCurrency[secondaryCurrency];
+    saveValues();
+	leftInput = document.getElementById('leftInput');
+	leftInput.value = (amount * rate).toFixed(2);
 }
 
-function getLastConvertion() {
-    currencyLeft = document.getElementById('currencyLeft');
-    currencyRight = document.getElementById('currencyRight');
-    amount = document.getElementById('floatingInputLeft');
-    
-	var newLeftCurrency = JSON.parse(localStorage.getItem('mainCurrency'));
-    var newRightCurrency = localStorage.getItem('secondaryCurrency');
-    var newAmount = localStorage.getItem('amount');
-	currencyLeft.value = newLeftCurrency.name;
-    currencyRight.value = newRightCurrency;
-    amount.value = newAmount;
-	convertionFromLeft();
-}
+// Events associated with the Select
+let leftCurrency = document.getElementById('leftCurrency');
+leftCurrency.onchange = () => fromRight();
+let rightCurrency = document.getElementById('rightCurrency');
+rightCurrency.onchange = () => fromLeft();
 
-let currencyLeft = document.getElementById('currencyLeft');
-currencyLeft.addEventListener('change', changeLabelLeft);
-let currencyRight = document.getElementById('currencyRight');
-currencyRight.addEventListener('change', changeLabelRight);
+// Events associated with the Input
+let leftInput = document.getElementById('leftInput');
+leftInput.onkeyup = () => fromLeft();
+let rightInput = document.getElementById('rightInput');
+rightInput.onkeyup = () => fromRight();
 
-let floatingInputLeft = document.getElementById('floatingInputLeft');
-floatingInputLeft.addEventListener('keyup', convertionFromLeft);
-
-let floatingInputRight = document.getElementById('floatingInputRight');
-floatingInputRight.addEventListener('keyup', convertionFromRight);
-
+// Events to get last conversion
 let lastConvertion = document.getElementById('lastConvertion');
-lastConvertion.addEventListener('click', getLastConvertion);
+lastConvertion.onclick = () => {
+    let leftCurrency = document.getElementById('leftCurrency');
+    let rightCurrency = document.getElementById('rightCurrency');
+    amount = document.getElementById('leftInput');
+    
+    var lastLeftCurrency = JSON.parse(localStorage.getItem('mainCurrency'));
+    var lastRightCurrency = localStorage.getItem('secondaryCurrency');
+    var lastAmount = localStorage.getItem('amount');
+    leftCurrency.value = lastLeftCurrency.name;
+    rightCurrency.value = lastRightCurrency;
+    amount.value = lastAmount;
+    fromLeft();
+};
+
+// Event to cross the currencies
+let crossBtn = document.getElementById('btnRefresh');
+crossBtn.onmousedown = refreshBtn;
+crossBtn.onclick = () => {
+	var leftCurrency = document.getElementById('leftCurrency');
+	var rightCurrency = document.getElementById('rightCurrency');
+
+	var newRightCurrencyValue = leftCurrency.value;
+	var newLeftCurrencyValue = rightCurrency.value;
+	leftCurrency.value = newLeftCurrencyValue;
+	rightCurrency.value = newRightCurrencyValue;
+	fromLeft();
+};
+
+// Cleanup event
+let cleanBtn = document.getElementById('clean');
+cleanBtn.onclick = () => {
+	leftInput = document.getElementById('leftInput');
+	leftInput.value = 0;
+	rightInput = document.getElementById('rightInput');
+	rightInput.value = 0;
+};
